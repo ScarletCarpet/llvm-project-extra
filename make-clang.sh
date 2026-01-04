9c2a7b42dc2672e7de1a88df6b03616f38f73630
loongshen <islgshen@qq.com>
2026-01-04 21:21:50 +0800

    add static build

--- make-clang.sh
+++ make-clang.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
+set -e
+
 mkdir -p build
 cd build
 
-if [ "$1" = "win"]; then
-    cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" ../llvm
+if [ "$1" = "win" ]; then
+    echo "making windows clang tools"
+    cmake -G "MinGW Makefiles" \
+        -DCMAKE_BUILD_TYPE=Release\
+        -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra"\
+        -DLLVM_STATIC_CLANG_FORMAT\
+        -DLLVM_STATIC_CLANG_TIDY\
+        ../llvm
     mingw32-make.exe -j8
 else
-    cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" ../llvm
+    echo "making unix clang tools"
+    cmake -G "Ninja" \
+        -DCMAKE_BUILD_TYPE=Release\
+        -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra"\
+        -DLLVM_STATIC_CLANG_FORMAT\
+        -DLLVM_STATIC_CLANG_TIDY\
+        ../llvm
     ninja -j8
 fi
-
