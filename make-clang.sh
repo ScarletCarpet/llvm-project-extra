418b0016e0ad58df8799a1eea04d3c8a190b8109
loongshen <islgshen@qq.com>
2026-01-05 20:04:34 +0800

    fix make

--- make-clang.sh
+++ make-clang.sh
@@ -7,19 +7,19 @@ cd build
 if [ "$1" = "win" ]; then
     echo "making windows clang tools"
     cmake -G "MinGW Makefiles" \
-        -DCMAKE_BUILD_TYPE=Release\
+        -DCMAKE_BUILD_TYPE=$2\
         -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra"\
-        -DLLVM_STATIC_CLANG_FORMAT\
-        -DLLVM_STATIC_CLANG_TIDY\
+        -DLLVM_STATIC_CLANG_FORMAT=1\
+        -DLLVM_STATIC_CLANG_TIDY=1\
         ../llvm
     mingw32-make.exe -j8
 else
     echo "making unix clang tools"
     cmake -G "Ninja" \
-        -DCMAKE_BUILD_TYPE=Release\
+        -DCMAKE_BUILD_TYPE=$2\
         -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra"\
-        -DLLVM_STATIC_CLANG_FORMAT\
-        -DLLVM_STATIC_CLANG_TIDY\
+        -DLLVM_STATIC_CLANG_FORMAT=1\
+        -DLLVM_STATIC_CLANG_TIDY=1\
         ../llvm
     ninja -j8
 fi
